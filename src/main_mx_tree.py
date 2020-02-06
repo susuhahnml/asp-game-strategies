52a7837b2e123cab7b9553726e8d1dae7169c650
Atreya Shankar <shankar.atreya@gmail.com>
2020-02-06 17:04:39 +0100

    *shankar* next branch init

--- src/main_mx_tree.py
+++ src/main_mx_tree.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import re
+import argparse
 from py_utils import arg_metav_formatter
 from game_definitions import *
-import argparse
 from structures.tree import Tree
-from py_utils.logger import log 
+from py_utils.logger import log
+
 def main_tree(plaintext,file_name,main_player,game_name):
     # remove trailing backslash as failsafe
     html = not plaintext
@@ -16,7 +17,6 @@ def main_tree(plaintext,file_name,main_player,game_name):
     tree.from_game_def(game,main_player=main_player)
     tree.print_in_file(html=html,file_name=file_name,main_player=main_player)
     log.info("Tree image saved in {}".format(file_name))
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=arg_metav_formatter)
@@ -30,13 +30,9 @@ if __name__ == "__main__":
                     help="the player from wich to maximize")
     parser.add_argument("--game-name", type=str, default="Nim",
                 help="short name for the game. Available: Dom and Nim")
-
-
     # parser.add_argument("--random-seed", type=int, default=0,
                         # help="the random seed for the initial state, 0 indicates the use of default initial state")
-
     args = parser.parse_args()
     log.set_level(args.log)
-
     # run tree command
     main_tree(args.plaintext,args.image_file_name,args.main_player,args.game_name)
--- src/tests/test_tree.py
+++ src/tests/test_tree.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from structures.tree import Tree
-from ..game_definitions.game_def import GameNimDef
+from game_definitions.game_def import GameNimDef
 
 def test_tree():
     tree = Tree()
-    nim = GameNimDef()
+    nim = GameNimDef("./game_definitions/test_nim")
     tree.from_game_def(nim)
     tree.print_in_file(html=True,file_name="test_tree_html.png")
     tree.print_in_file(html=False,file_name="test_tree_plain.png")
